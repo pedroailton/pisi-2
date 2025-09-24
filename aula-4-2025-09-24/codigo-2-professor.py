963fcfcd9ed1ffc47d81e490a7d5014044007ebe
Pedro Ailton <pedroailton630@gmail.com>
2025-09-24 11:41:43 -0300

    Fim da Aula

--- aula-4-2025-09-24/codigo-2-professor.py
+++ aula-4-2025-09-24/codigo-2-professor.py
@@ -1,5 +1,23 @@
+"""
 def buscaBinaria(lista, alvo):
     inicio = 0
+    fim = len(lista) - 1 ## Índice do último elemento (sem considerar o elemento 0 como quantidade)
+
+    ## ITERATIVO
+    while inicio <= fim:
+        meio = (inicio + fim) // 2 ## Arrendonda para baixo
+        if lista[meio] == alvo: ## Caso base da recursão
+            return meio
+        elif lista[meio] < alvo:
+            inicio = meio + 1
+        else:
+            fim = meio - 1
+
+    return -1  ## Elemento não encontrado
+"""
+
+    ## RECURSIVO
+def buscaBinariaRecursiva(lista, alvo, inicio, fim):
     fim = len(lista) - 1 ## Índice do último elemento (sem considerar o elemento 0 como quantidade)
 
     while inicio <= fim:
@@ -12,6 +30,7 @@ def buscaBinaria(lista, alvo):
             fim = meio - 1
 
     return -1  ## Elemento não encontrado
+
 
 # Programa Principal
 from random import randint, shuffle
